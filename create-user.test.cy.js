19fdf5493ca6e7a02e9d9b789622d8331806b161
Robi Pradana <robipradana28@gmail.com>
2024-01-19 11:44:00 +0700

    upload file create-user.test.cy.js

--- create-user.test.cy.js
+++ create-user.test.cy.js
@@ -7,6 +7,8 @@ describe ('Create New List', () => {
         assert.isObject(user, 'value is object')
         cy.request('POST', 'https://reqres.in/api/users', user).then((response) => {
             expect(response.status).equal(201)
+            expect(response.body.name).to.eq('Eduwork')
+            expect(response.body.job).to.eq('Education')
         })
     })
     })
